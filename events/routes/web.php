d309b4139f2ef1dec3214b90c889eb683c4641f8
zahid202232 <101721745+zahid202232@users.noreply.github.com>
2022-10-03 17:39:51 -0700

    checkout

--- events/routes/web.php
+++ events/routes/web.php
@@ -88,3 +88,4 @@ Route::post('company/password/{token}', [App\Http\Controllers\Admin\CompanyContr
 
 Route::get('setting', [App\Http\Controllers\Admin\SettingController::class, 'index'])->name('admin.setting.index');
 Route::get('menu', [App\Http\Controllers\Admin\SettingController::class, 'menu'])->name('admin.setting.menu');
+Route::get('checkout', [App\Http\Controllers\Admin\SettingController::class, 'menu'])->name('admin.checkout');
